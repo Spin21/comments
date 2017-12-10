5a0f46ebb180d98d846036d47286fe2210c458a8
Stefan Tórz <storz@interia.eu>
2017-12-10 19:57:26 +0100

    add js file with comments

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// scripts.js
+
+var name = prompt('Enter your name');
+alert('Hello, ' + name); //this is the first comment
+console.log('Hello, ' + name); //this is the second comment
+/* This is the comment 
+in the block*/
